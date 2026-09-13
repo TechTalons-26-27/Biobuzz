2a3b52f1b2b8eaad189ebe976671ac134ba72c3b
shruthi-l09 <saishruthilaks@gmail.com>
2026-09-13 15:43:43 -0700

    Update auto1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/auto1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode.autoPaths; // make sure this aligns with class location
+package org.firstinspires.ftc.teamcode.opmode.auto; // make sure this aligns with class location
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
